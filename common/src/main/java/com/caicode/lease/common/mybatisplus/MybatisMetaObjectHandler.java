f07fc16353401234988b54ff66942c43296034aa
caicode123 <127097912+caicode123@users.noreply.github.com>
2024-12-23 14:15:56 +0800

    增加创建时间和更新时间

--- common/src/main/java/com/caicode/lease/common/mybatisplus/MybatisMetaObjectHandler.java
+++ common/src/main/java/com/caicode/lease/common/mybatisplus/MybatisMetaObjectHandler.java
@@ -8,11 +8,18 @@ import java.util.Date;
 
 @Component
 public class MybatisMetaObjectHandler implements MetaObjectHandler {
+
+    /*
+     * 插入数据时自动填充
+     */
     @Override
     public void insertFill(MetaObject metaObject) {
         this.strictInsertFill(metaObject, "createTime", Date.class, new Date());
     }
 
+    /*
+     * 更新数据时自动填充
+     */
     @Override
     public void updateFill(MetaObject metaObject) {
         this.strictUpdateFill(metaObject, "updateTime", Date.class, new Date());
