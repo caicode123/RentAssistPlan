ef80118e74229348a34087acbed062a1d7d8b5d9
WangJihao <3091169959@qq.com>
2024-12-23 16:09:43 +0800

    标签名模块以及修复无标签值导致标签名无法显示的bug

--- web/web-admin/src/main/java/com/caicode/lease/web/admin/mapper/AttrKeyMapper.java
+++ web/web-admin/src/main/java/com/caicode/lease/web/admin/mapper/AttrKeyMapper.java
@@ -3,6 +3,7 @@ package com.caicode.lease.web.admin.mapper;
 import com.caicode.lease.model.entity.AttrKey;
 import com.caicode.lease.web.admin.vo.attr.AttrKeyVo;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
@@ -11,6 +12,7 @@ import java.util.List;
 * @description 针对表【attr_key(房间基本属性表)】的数据库操作Mapper
 * @Entity com.caicode.lease.model.AttrKey
 */
+@Mapper
 public interface AttrKeyMapper extends BaseMapper<AttrKey> {
 
     List<AttrKeyVo> listAttrInfo();
