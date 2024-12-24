eea816c64b4425384f1a8f712806f7556808ff59
caicode123 <127097912+caicode123@users.noreply.github.com>
2024-12-24 16:56:32 +0800

    保存或更新房间信息

--- web/web-admin/src/main/java/com/caicode/lease/web/admin/service/RoomInfoService.java
+++ web/web-admin/src/main/java/com/caicode/lease/web/admin/service/RoomInfoService.java
@@ -9,10 +9,10 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.IService;
 
 /**
-* @author liubo
+* @author caicode123
 * @description 针对表【room_info(房间信息表)】的数据库操作Service
-* @createDate 2023-07-24 15:48:00
 */
 public interface RoomInfoService extends IService<RoomInfo> {
 
+    void saveOrUpdateRoom(RoomSubmitVo roomSubmitVo);
 }
