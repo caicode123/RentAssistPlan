2e9851f3904d82a9907622b7d2c3d280e0e85bcf
WangJihao <3091169959@qq.com>
2024-12-25 15:24:01 +0800

    登录图形验证码生成

--- web/web-admin/src/main/java/com/caicode/lease/web/admin/service/LoginService.java
+++ web/web-admin/src/main/java/com/caicode/lease/web/admin/service/LoginService.java
@@ -6,4 +6,5 @@ import com.caicode.lease.web.admin.vo.system.user.SystemUserInfoVo;
 
 public interface LoginService {
 
+    CaptchaVo getCaptcha();
 }
--- web/web-admin/src/main/java/com/caicode/lease/web/admin/service/impl/LoginServiceImpl.java
+++ web/web-admin/src/main/java/com/caicode/lease/web/admin/service/impl/LoginServiceImpl.java
@@ -1,9 +1,34 @@
 package com.caicode.lease.web.admin.service.impl;
 
+import com.caicode.lease.common.constant.RedisConstant;
 import com.caicode.lease.web.admin.service.LoginService;
+import com.caicode.lease.web.admin.vo.login.CaptchaVo;
+import com.wf.captcha.SpecCaptcha;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class LoginServiceImpl implements LoginService {
 
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public CaptchaVo getCaptcha() {
+        //创建验证码
+        SpecCaptcha specCaptcha = new SpecCaptcha(130, 48, 4);
+
+        //不区分大小写
+        String code = specCaptcha.text().toLowerCase();
+        //生成UUID
+        String key = RedisConstant.ADMIN_LOGIN_PREFIX + UUID.randomUUID();
+        //将验证码存入redis
+        stringRedisTemplate.opsForValue().set(key, code,RedisConstant.ADMIN_LOGIN_CAPTCHA_TTL_SEC, TimeUnit.SECONDS);
+
+        return new CaptchaVo(specCaptcha.toBase64(),key);
+    }
 }
