8ab6d018cf2e614842aefc6be1116816b29a90fd
caicode123 <127097912+caicode123@users.noreply.github.com>
2024-12-26 16:48:25 +0800

    根据房间ID获取可选租期实现

--- web/web-app/src/main/java/com/caicode/lease/web/app/service/LeaseTermService.java
+++ web/web-app/src/main/java/com/caicode/lease/web/app/service/LeaseTermService.java
@@ -3,9 +3,12 @@ package com.caicode.lease.web.app.service;
 import com.caicode.lease.model.entity.LeaseTerm;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.List;
+
 /**
 * @author caicode
 * @description 针对表【lease_term(租期)】的数据库操作Service
 */
 public interface LeaseTermService extends IService<LeaseTerm> {
+    List<LeaseTerm> listByRoomId(Long id);
 }
