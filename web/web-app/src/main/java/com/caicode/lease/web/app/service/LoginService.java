5e54bb0bd6edd805ed27b7b42774ff49c63f82d6
caicode123 <127097912+caicode123@users.noreply.github.com>
2024-12-26 14:20:51 +0800

    登录注册模块实现

--- web/web-app/src/main/java/com/caicode/lease/web/app/service/LoginService.java
+++ web/web-app/src/main/java/com/caicode/lease/web/app/service/LoginService.java
@@ -1,5 +1,9 @@
 package com.caicode.lease.web.app.service;
 
+import com.caicode.lease.web.app.vo.user.LoginVo;
+
 public interface LoginService {
     void getCode(String phone);
+
+    String login(LoginVo loginVo);
 }
--- web/web-app/src/main/java/com/caicode/lease/web/app/service/impl/LoginServiceImpl.java
+++ web/web-app/src/main/java/com/caicode/lease/web/app/service/impl/LoginServiceImpl.java
@@ -1,11 +1,18 @@
 package com.caicode.lease.web.app.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.caicode.lease.common.constant.RedisConstant;
 import com.caicode.lease.common.exception.LeaseException;
 import com.caicode.lease.common.result.ResultCodeEnum;
 import com.caicode.lease.common.utils.CodeUtil;
+import com.caicode.lease.common.utils.JwtUtil;
+import com.caicode.lease.model.entity.BaseEntity;
+import com.caicode.lease.model.entity.UserInfo;
+import com.caicode.lease.model.enums.BaseStatus;
+import com.caicode.lease.web.app.mapper.UserInfoMapper;
 import com.caicode.lease.web.app.service.LoginService;
 import com.caicode.lease.web.app.service.SmsService;
+import com.caicode.lease.web.app.vo.user.LoginVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
@@ -20,6 +27,9 @@ public class LoginServiceImpl implements LoginService {
 
     @Autowired
     private StringRedisTemplate redisTemplate;
+
+    @Autowired
+    private UserInfoMapper userInfoMapper;
 
     @Override
     public void getCode(String phone) {
@@ -39,5 +49,52 @@ public class LoginServiceImpl implements LoginService {
         //验证码存入redis
         smsService.sendCode(phone, randomCode);
         redisTemplate.opsForValue().set(ksy, randomCode, RedisConstant.APP_LOGIN_CODE_TTL_SEC, TimeUnit.SECONDS);
+    }
+
+    @Override
+    public String login(LoginVo loginVo) {
+
+        //验证手机号不为空
+        if (loginVo.getPhone() == null){
+            throw new LeaseException(ResultCodeEnum.APP_LOGIN_PHONE_EMPTY);
+        }
+
+        //验证验证码不为空
+        if (loginVo.getCode() == null){
+            throw new LeaseException(ResultCodeEnum.APP_LOGIN_CODE_EMPTY);
+        }
+
+        //拿到key和code
+        String key = RedisConstant.APP_LOGIN_PREFIX + loginVo.getPhone();
+        String code = redisTemplate.opsForValue().get(key);
+
+        //验证验证码是否正确
+        if (code == null){
+            throw new LeaseException(ResultCodeEnum.APP_LOGIN_CODE_EXPIRED);
+        }
+        if (!code.equals(loginVo.getCode())){
+            throw new LeaseException(ResultCodeEnum.APP_LOGIN_CODE_ERROR);
+        }
+
+        //验证用户是否存在
+        LambdaQueryWrapper<UserInfo> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(UserInfo::getPhone, loginVo.getPhone());
+        UserInfo userInfo = userInfoMapper.selectOne(queryWrapper);
+        if (userInfo == null){
+           //用户不存在则进行注册
+            userInfo = new UserInfo();
+            userInfo.setPhone(loginVo.getPhone());
+            userInfo.setStatus(BaseStatus.ENABLE);
+            //默认昵称为用户-手机尾号后四位
+            userInfo.setNickname("用户-"+loginVo.getPhone().substring(7));
+            userInfoMapper.insert(userInfo);
+        }
+        else {
+            //验证用户是否被禁用
+            if (userInfo.getStatus() == BaseStatus.DISABLE){
+                throw new LeaseException(ResultCodeEnum.APP_ACCOUNT_DISABLED_ERROR);
+            }
+        }
+        return JwtUtil.createToken(userInfo.getId(), userInfo.getPhone());
     }
 }
