04fcd66b3c93ddfbaef41cb3ac717f064751a72e
caicode123 <127097912+caicode123@users.noreply.github.com>
2024-12-26 15:31:57 +0800

    根据条件分页查询房间列表实现

--- web/web-app/src/main/java/com/caicode/lease/web/app/service/RoomInfoService.java
+++ web/web-app/src/main/java/com/caicode/lease/web/app/service/RoomInfoService.java
@@ -1,11 +1,16 @@
 package com.caicode.lease.web.app.service;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.caicode.lease.model.entity.RoomInfo;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.caicode.lease.web.app.vo.room.RoomItemVo;
+import com.caicode.lease.web.app.vo.room.RoomQueryVo;
 
 /**
 * @author caicode
 * @description 针对表【room_info(房间信息表)】的数据库操作Service
 */
 public interface RoomInfoService extends IService<RoomInfo> {
+    IPage<RoomItemVo> pageItem(Page<RoomItemVo> page, RoomQueryVo queryVo);
 }
--- web/web-app/src/main/java/com/caicode/lease/web/app/service/impl/RoomInfoServiceImpl.java
+++ web/web-app/src/main/java/com/caicode/lease/web/app/service/impl/RoomInfoServiceImpl.java
@@ -1,10 +1,15 @@
 package com.caicode.lease.web.app.service.impl;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.caicode.lease.model.entity.RoomInfo;
 import com.caicode.lease.web.app.mapper.RoomInfoMapper;
 import com.caicode.lease.web.app.service.RoomInfoService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.caicode.lease.web.app.vo.room.RoomItemVo;
+import com.caicode.lease.web.app.vo.room.RoomQueryVo;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -16,6 +21,13 @@ import org.springframework.stereotype.Service;
 public class RoomInfoServiceImpl extends ServiceImpl<RoomInfoMapper, RoomInfo>
         implements RoomInfoService {
 
+    @Autowired
+    private RoomInfoMapper roomInfoMapper;
+
+    @Override
+    public IPage<RoomItemVo> pageItem(Page<RoomItemVo> page, RoomQueryVo queryVo) {
+        return roomInfoMapper.pageItem(page, queryVo);
+    }
 }
 
 
